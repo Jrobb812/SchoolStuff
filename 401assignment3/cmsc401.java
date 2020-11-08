a5f08198887468bedd3f566e8499e8ce99666b8f
Jrobb812 <robbinsja2@vcu.edu>
2020-11-07 19:35:44 -0500

    new commit

---
+++ 401assignment3/cmsc401.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+public class cmsc401 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input num cities");
+        int V = scan.nextInt(); //number of verticies
+        int E = scan.nextInt(); //number of edges
+
+    }
+    public int dijkstra(int[][] graph, int src) {
+
+        return 0;
+    }
+    public int[][] createList() {
+        
+        return 0;
+    }
+}
